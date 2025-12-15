fb746e099b5438c1b8bec6c67da112b4f0a7ded1
ogrenci <ogrenciogrenci402@proton.me>
2025-12-15 12:47:02 +0200

    Finished designing form3

--- kutuphane/Form3.Designer.cs
+++ kutuphane/Form3.Designer.cs
@@ -30,7 +30,9 @@
         {
             tableLayoutPanel2 = new TableLayoutPanel();
             button3 = new Button();
+            button8 = new Button();
             btnyenile = new Button();
+            button7 = new Button();
             DataGridView_kitaplar = new DataGridView();
             dataGridView1 = new DataGridView();
             pictureBox1 = new PictureBox();
@@ -39,13 +41,25 @@
             button4 = new Button();
             button5 = new Button();
             button6 = new Button();
-            button7 = new Button();
-            button8 = new Button();
             tableLayoutPanel1 = new TableLayoutPanel();
             button1 = new Button();
             button2 = new Button();
             button9 = new Button();
             button10 = new Button();
+            label_ad = new Label();
+            label_sifre = new Label();
+            label_rol = new Label();
+            label_kitap_adi = new Label();
+            label_yayin_evi = new Label();
+            label_basim_yili = new Label();
+            richTextBox1 = new RichTextBox();
+            richTextBox2 = new RichTextBox();
+            richTextBox3 = new RichTextBox();
+            richTextBox4 = new RichTextBox();
+            richTextBox5 = new RichTextBox();
+            richTextBox6 = new RichTextBox();
+            richTextBox7 = new RichTextBox();
+            label_tur = new Label();
             tableLayoutPanel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)DataGridView_kitaplar).BeginInit();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
@@ -55,22 +69,31 @@
             // 
             // tableLayoutPanel2
             // 
+            tableLayoutPanel2.Anchor = AnchorStyles.None;
             tableLayoutPanel2.ColumnCount = 2;
             tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 46.40288F));
             tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 53.59712F));
+            tableLayoutPanel2.Controls.Add(label_tur, 0, 3);
+            tableLayoutPanel2.Controls.Add(richTextBox6, 1, 2);
+            tableLayoutPanel2.Controls.Add(richTextBox5, 1, 1);
+            tableLayoutPanel2.Controls.Add(richTextBox4, 1, 0);
             tableLayoutPanel2.Controls.Add(button3, 1, 5);
             tableLayoutPanel2.Controls.Add(button8, 0, 5);
             tableLayoutPanel2.Controls.Add(btnyenile, 0, 4);
             tableLayoutPanel2.Controls.Add(button7, 1, 4);
+            tableLayoutPanel2.Controls.Add(label_yayin_evi, 0, 1);
+            tableLayoutPanel2.Controls.Add(label_basim_yili, 0, 2);
+            tableLayoutPanel2.Controls.Add(richTextBox7, 1, 3);
+            tableLayoutPanel2.Controls.Add(label_kitap_adi, 0, 0);
             tableLayoutPanel2.Location = new Point(12, 82);
             tableLayoutPanel2.Name = "tableLayoutPanel2";
             tableLayoutPanel2.RowCount = 6;
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 29F));
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 23F));
-            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 18F));
-            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 38F));
-            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 36F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 24F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 37F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 31F));
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
             tableLayoutPanel2.Size = new Size(278, 171);
             tableLayoutPanel2.TabIndex = 13;
@@ -81,24 +104,48 @@
             button3.BackColor = Color.Red;
             button3.Font = new Font("Comic Sans MS", 12F, FontStyle.Bold | FontStyle.Italic);
             button3.ForeColor = SystemColors.Control;
-            button3.Location = new Point(132, 131);
+            button3.Location = new Point(132, 136);
             button3.Name = "button3";
-            button3.Size = new Size(143, 37);
+            button3.Size = new Size(143, 32);
             button3.TabIndex = 7;
             button3.Text = "Leave";
             button3.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.Navy;
+            button8.Font = new Font("Comic Sans MS", 12F, FontStyle.Bold | FontStyle.Italic);
+            button8.ForeColor = SystemColors.Control;
+            button8.Location = new Point(3, 136);
+            button8.Name = "button8";
+            button8.Size = new Size(123, 30);
+            button8.TabIndex = 23;
+            button8.Text = "Update";
+            button8.UseVisualStyleBackColor = false;
             // 
             // btnyenile
             // 
             btnyenile.BackColor = Color.LimeGreen;
             btnyenile.Font = new Font("Comic Sans MS", 12F, FontStyle.Bold | FontStyle.Italic);
             btnyenile.ForeColor = SystemColors.Control;
-            btnyenile.Location = new Point(3, 93);
+            btnyenile.Location = new Point(3, 99);
             btnyenile.Name = "btnyenile";
-            btnyenile.Size = new Size(123, 32);
+            btnyenile.Size = new Size(123, 31);
             btnyenile.TabIndex = 10;
             btnyenile.Text = "Enter";
             btnyenile.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.Goldenrod;
+            button7.Font = new Font("Comic Sans MS", 12F, FontStyle.Bold | FontStyle.Italic);
+            button7.ForeColor = SystemColors.Control;
+            button7.Location = new Point(132, 99);
+            button7.Name = "button7";
+            button7.Size = new Size(143, 31);
+            button7.TabIndex = 11;
+            button7.Text = "Clean";
+            button7.UseVisualStyleBackColor = false;
             // 
             // DataGridView_kitaplar
             // 
@@ -184,39 +231,21 @@
             button6.Text = "Logout";
             button6.UseVisualStyleBackColor = false;
             // 
-            // button7
-            // 
-            button7.BackColor = Color.Goldenrod;
-            button7.Font = new Font("Comic Sans MS", 12F, FontStyle.Bold | FontStyle.Italic);
-            button7.ForeColor = SystemColors.Control;
-            button7.Location = new Point(132, 93);
-            button7.Name = "button7";
-            button7.Size = new Size(143, 32);
-            button7.TabIndex = 11;
-            button7.Text = "Clean";
-            button7.UseVisualStyleBackColor = false;
-            // 
-            // button8
-            // 
-            button8.BackColor = Color.Navy;
-            button8.Font = new Font("Comic Sans MS", 12F, FontStyle.Bold | FontStyle.Italic);
-            button8.ForeColor = SystemColors.Control;
-            button8.Location = new Point(3, 131);
-            button8.Name = "button8";
-            button8.Size = new Size(123, 30);
-            button8.TabIndex = 23;
-            button8.Text = "Update";
-            button8.UseVisualStyleBackColor = false;
-            // 
             // tableLayoutPanel1
             // 
             tableLayoutPanel1.ColumnCount = 2;
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 46.40288F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 53.59712F));
+            tableLayoutPanel1.Controls.Add(richTextBox3, 1, 1);
+            tableLayoutPanel1.Controls.Add(richTextBox2, 1, 0);
             tableLayoutPanel1.Controls.Add(button1, 1, 4);
             tableLayoutPanel1.Controls.Add(button2, 0, 4);
             tableLayoutPanel1.Controls.Add(button9, 0, 3);
             tableLayoutPanel1.Controls.Add(button10, 1, 3);
+            tableLayoutPanel1.Controls.Add(label_rol, 0, 2);
+            tableLayoutPanel1.Controls.Add(label_sifre, 0, 1);
+            tableLayoutPanel1.Controls.Add(label_ad, 0, 0);
+            tableLayoutPanel1.Controls.Add(richTextBox1, 1, 2);
             tableLayoutPanel1.Location = new Point(12, 259);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 5;
@@ -228,6 +257,7 @@
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
             tableLayoutPanel1.Size = new Size(278, 171);
             tableLayoutPanel1.TabIndex = 23;
+            tableLayoutPanel1.Paint += tableLayoutPanel1_Paint;
             // 
             // button1
             // 
@@ -277,6 +307,130 @@
             button10.Text = "Clean";
             button10.UseVisualStyleBackColor = false;
             // 
+            // label_ad
+            // 
+            label_ad.Anchor = AnchorStyles.Left;
+            label_ad.AutoSize = true;
+            label_ad.Location = new Point(3, 7);
+            label_ad.Name = "label_ad";
+            label_ad.Size = new Size(39, 15);
+            label_ad.TabIndex = 24;
+            label_ad.Text = "Name";
+            // 
+            // label_sifre
+            // 
+            label_sifre.Anchor = AnchorStyles.Left;
+            label_sifre.Location = new Point(3, 33);
+            label_sifre.Name = "label_sifre";
+            label_sifre.Size = new Size(100, 23);
+            label_sifre.TabIndex = 25;
+            label_sifre.Text = "Password";
+            // 
+            // label_rol
+            // 
+            label_rol.Anchor = AnchorStyles.Left;
+            label_rol.AutoSize = true;
+            label_rol.Location = new Point(3, 68);
+            label_rol.Name = "label_rol";
+            label_rol.Size = new Size(30, 15);
+            label_rol.TabIndex = 26;
+            label_rol.Text = "Role";
+            // 
+            // label_kitap_adi
+            // 
+            label_kitap_adi.Anchor = AnchorStyles.Left;
+            label_kitap_adi.AutoSize = true;
+            label_kitap_adi.Location = new Point(3, 2);
+            label_kitap_adi.Name = "label_kitap_adi";
+            label_kitap_adi.Size = new Size(69, 15);
+            label_kitap_adi.TabIndex = 27;
+            label_kitap_adi.Text = "Book Name";
+            // 
+            // label_yayin_evi
+            // 
+            label_yayin_evi.Anchor = AnchorStyles.Left;
+            label_yayin_evi.AutoSize = true;
+            label_yayin_evi.Location = new Point(3, 27);
+            label_yayin_evi.Name = "label_yayin_evi";
+            label_yayin_evi.Size = new Size(100, 15);
+            label_yayin_evi.TabIndex = 28;
+            label_yayin_evi.Text = "Publishing House";
+            // 
+            // label_basim_yili
+            // 
+            label_basim_yili.Anchor = AnchorStyles.Left;
+            label_basim_yili.AutoSize = true;
+            label_basim_yili.Location = new Point(3, 53);
+            label_basim_yili.Name = "label_basim_yili";
+            label_basim_yili.Size = new Size(86, 15);
+            label_basim_yili.TabIndex = 29;
+            label_basim_yili.Text = "Published Date";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(132, 63);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(143, 26);
+            richTextBox1.TabIndex = 27;
+            richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Location = new Point(132, 3);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(143, 23);
+            richTextBox2.TabIndex = 28;
+            richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            richTextBox3.Location = new Point(132, 32);
+            richTextBox3.Name = "richTextBox3";
+            richTextBox3.Size = new Size(143, 25);
+            richTextBox3.TabIndex = 29;
+            richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            richTextBox4.Location = new Point(132, 3);
+            richTextBox4.Name = "richTextBox4";
+            richTextBox4.Size = new Size(143, 14);
+            richTextBox4.TabIndex = 30;
+            richTextBox4.Text = "";
+            // 
+            // richTextBox5
+            // 
+            richTextBox5.Location = new Point(132, 23);
+            richTextBox5.Name = "richTextBox5";
+            richTextBox5.Size = new Size(143, 23);
+            richTextBox5.TabIndex = 31;
+            richTextBox5.Text = "  ";
+            // 
+            // richTextBox6
+            // 
+            richTextBox6.Location = new Point(132, 52);
+            richTextBox6.Name = "richTextBox6";
+            richTextBox6.Size = new Size(143, 17);
+            richTextBox6.TabIndex = 32;
+            richTextBox6.Text = "";
+            // 
+            // richTextBox7
+            // 
+            richTextBox7.Location = new Point(132, 75);
+            richTextBox7.Name = "richTextBox7";
+            richTextBox7.Size = new Size(143, 18);
+            richTextBox7.TabIndex = 33;
+            richTextBox7.Text = "";
+            // 
+            // label_tur
+            // 
+            label_tur.Anchor = AnchorStyles.Left;
+            label_tur.Location = new Point(3, 76);
+            label_tur.Name = "label_tur";
+            label_tur.Size = new Size(38, 15);
+            label_tur.TabIndex = 34;
+            label_tur.Text = "Type";
+            // 
             // Form3
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -296,10 +450,12 @@
             Text = "Form3";
             Load += Form3_Load;
             tableLayoutPanel2.ResumeLayout(false);
+            tableLayoutPanel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)DataGridView_kitaplar).EndInit();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -324,5 +480,25 @@
         private Button button2;
         private Button button9;
         private Button button10;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private RichTextBox richTextBox6;
+        private RichTextBox richTextBox5;
+        private RichTextBox richTextBox4;
+        private RichTextBox richTextBox7;
+        private RichTextBox richTextBox3;
+        private RichTextBox richTextBox2;
+        private Label label_rol;
+        private Label label_sifre;
+        private Label label_ad;
+        private RichTextBox richTextBox1;
+        private Label label_tur;
+        private Label label_kitap_adi;
+        private Label label_yayin_evi;
+        private Label label_basim_yili;
     }
 }
--- kutuphane/Form3.cs
+++ kutuphane/Form3.cs
@@ -26,5 +26,10 @@ namespace kutuphane
         {
 
         }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
